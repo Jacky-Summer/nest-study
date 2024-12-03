061e8b0d61bc7ce68309101438c606055ec5a9f9
jack.lin <jack.lin@imile.me>
2024-12-03 23:40:57 +0800

    feat: 支持静态资源public目录的访问

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { PersonModule } from './person/person.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, PersonModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets('public', { prefix: '/static' }); // 支持静态资源的请求
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
