f4cc1d4b33ecc5b31baf1edd34188546185eaae1
jack.lin <jack.lin@imile.me>
2024-12-04 22:56:26 +0800

    feat: add corresponding examples for each request type

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -6,14 +6,52 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
+  UseInterceptors,
+  UploadedFiles,
 } from '@nestjs/common';
 import { PersonService } from './person.service';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
 
-@Controller('person')
+export class CreatePersonDtoProps {
+  name: string;
+  age: number;
+}
+
+@Controller('api/person')
 export class PersonController {
   constructor(private readonly personService: PersonService) {}
+
+  @Get(':id')
+  urlParam(@Param('id') id: string) {
+    return `received: id=${id}`;
+  }
+
+  @Get('find')
+  query(@Query('name') name: string, @Query('age') age: number) {
+    return `received: name=${name},age=${age}`;
+  }
+
+  @Post()
+  body(@Body() createPersonDto: CreatePersonDtoProps) {
+    return `received: ${JSON.stringify(createPersonDto)}`;
+  }
+
+  @Post('file')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      dest: 'uploads/',
+    }),
+  )
+  body2(
+    @Body() createPersonDto: CreatePersonDto,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+  ) {
+    console.log(files);
+    return `received: ${JSON.stringify(createPersonDto)}`;
+  }
 
   @Post()
   create(@Body() createPersonDto: CreatePersonDto) {
